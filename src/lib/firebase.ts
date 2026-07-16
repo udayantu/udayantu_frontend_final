b67c24b062c11e2c29f869d3bae3f1aec1aeca3f
Raunit <rravenger7@gmail.com>
2026-07-17 00:11:37 +0530

    Hardcode new Firebase config to bypass Vercel environment setting desync

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyB_zsC3GhCzR7m1yfmB0QGL7X-svEPGWzc",
+  authDomain: "udayantu-7748d.firebaseapp.com",
+  projectId: "udayantu-7748d",
+  storageBucket: "udayantu-7748d.firebasestorage.app",
+  messagingSenderId: "1025062155088",
+  appId: "1:1025062155088:web:352b33acbd7a777f6ef255",
+  measurementId: "G-72SX0Y89Y4"
 };
 
 // Initialize Firebase
